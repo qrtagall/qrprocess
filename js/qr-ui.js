0136cdde88703234a0f3e53ce8e8c3f330eee1ad
workaholic <chandan2002x@gmail.com>
2025-10-16 17:09:25 +0530

    a

--- js/qr-ui.js
+++ js/qr-ui.js
@@ -937,6 +937,15 @@ function formatPhoneNumber(rawNum) {
 }
 */
 
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
 function formatTextContent(text) {
     if (!text) return "";
 
